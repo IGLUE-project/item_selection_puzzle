e3eb5b9afdc2fd1cab5090f6ca946eac7e064b5d
Javier Ferreño <javifc67@gmail.com>
2025-11-20 10:17:38 +0100

    updated config example

--- config-example.js
+++ config-example.js
@@ -4,7 +4,30 @@ export let ESCAPP_APP_SETTINGS = {
   //Settings that can be specified by the authors
   skin: "STANDARD", //skin can be "STANDARD", "RETRO" or "FUTURISTIC".
   // backgroundImg: "NONE", //background can be "NONE" or a URL.
-
+  rounds: [
+    [{ label: "Item 1" }, { label: "Item 2" }, { label: "Item 3" }],
+    [
+      { label: "Item A" },
+      { label: "Item B" },
+      { label: "Item C" },
+      { label: "Item D" },
+      { label: "Item E" },
+      { label: "Item F" },
+      { label: "Item A" },
+      { label: "Item aaaaaaaaaaaaaaaaaaaaaa" },
+      { label: "Item C" },
+      { label: "Item D" },
+      { label: "Item E" },
+      { label: "Item F" },
+    ],
+    [
+      { img: "https://png.pngtree.com/png-clipart/20220111/original/pngtree-pear-png-image_7086642.png" },
+      { img: "https://cdn.pixabay.com/photo/2016/02/23/17/42/orange-1218158_1280.png" },
+      {
+        img: "https://images.vexels.com/media/users/3/131878/isolated/preview/384b201e3c4076950837a47f417fcddf-icono-de-manzana-roja.png",
+      },
+    ],
+  ],
   //Settings that will be automatically specified by the Escapp server
   locale: "es",
 
